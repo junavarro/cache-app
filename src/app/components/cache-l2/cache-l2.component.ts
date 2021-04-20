837d33b6e207a104bfa47b8bc8252bde67ef156c
Juan Navarro <jnavcamacho@gmail.com>
2021-04-19 19:14:25 -0600

    se agrega datos para l2

--- src/app/components/cache-l2/cache-l2.component.ts
+++ src/app/components/cache-l2/cache-l2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import InitData from 'src/app/data/initData';
+import { CacheL2Block } from 'src/app/models/Models';
 
 @Component({
   selector: 'app-cache-l2',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cache-l2.component.scss']
 })
 export class CacheL2Component implements OnInit {
+  initData = InitData;
+  source: (CacheL2Block & {P0: boolean,P1: boolean, P2: boolean, P3: boolean})[] = [];
+  displayedColumns: string[] = ['blockId', 'state', 'P0', 'P1', 'P2', 'P3', 'address', 'data'];
 
-  constructor() { }
+  constructor() {
+    this.source = this.initData.CacheL2.blocks.map(block => {
+      return { ...block ,P0: block.list[0], P1: block.list[1], P2: block.list[2], P3: block.list[3]}
+    })
+  }
 
   ngOnInit(): void {
   }
